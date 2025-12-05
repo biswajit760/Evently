c970dc49ae0de717d918cffa3c8829e7fc9278a6
Biswajit Mahanty <155170520+biswajit760@users.noreply.github.com>
2025-12-05 13:48:05 +0530

    fix: update clerk webhook username

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -14,7 +14,7 @@ const UserSchema = new Schema<IUser>(
   {
     clerkId: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-    username: { type: String, required: false, unique: true, trim: true },
+    username: { type: String, required: false, unique: false, trim: true },
     firstName: { type: String, required: false, trim: true },
     lastName: { type: String, required: false, trim: true },
     photo: { type: String, required: true },
